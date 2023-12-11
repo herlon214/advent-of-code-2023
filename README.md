7a2c8f2fea4757a092d48171edf624585aa23174
Herlon Aguiar <me@herlon.dev>
2023-12-11 07:37:05 +0100

    day 10

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # Advent Of Code 2023
 
-| Day | Note                                                | Stars        |
-| --- | --------------------------------------------------- | ------------ |
-| 1   | Contiguous substrings                               | :star::star: |
-| 2   | Basic parsing and filtering data                    | :star::star: |
-| 3   | Ranges                                              | :star::star: |
-| 4   | Basic parsing and ranges                            | :star::star: |
-| 5   | Ranges, couldn't figure out part 2                  | :star:       |
-| 6   | Nothing special but it ran slow in python (somehow) | :star::star: |
-| 7   | Basic sorting and using combinations for part 2     | :star::star: |
-| 8   | Least Common Multiple for part 2                    | :star::star: |
-| 9   | Fastest part2 ever, lol                             | :star::star: |
+| Day | Note                                                                            | Stars        |
+| --- | ------------------------------------------------------------------------------- | ------------ |
+| 1   | Contiguous substrings                                                           | :star::star: |
+| 2   | Basic parsing and filtering data                                                | :star::star: |
+| 3   | Ranges                                                                          | :star::star: |
+| 4   | Basic parsing and ranges                                                        | :star::star: |
+| 5   | Ranges, couldn't figure out part 2                                              | :star:       |
+| 6   | Nothing special but it ran slow in python (somehow)                             | :star::star: |
+| 7   | Basic sorting and using combinations for part 2                                 | :star::star: |
+| 8   | Least Common Multiple for part 2                                                | :star::star: |
+| 9   | Fastest part2 ever, lol                                                         | :star::star: |
+| 10  | BFS, part 2 you need flood fill but i'm too lazy to get around the corner cases | :star:       |
--- day-10/part2.py
+++ day-10/part2.py
@@ -2,8 +2,20 @@ from pprint import pprint
 import time
 import os
 
+markers = ["~", "?", "o"]
+
 # Read input from file
-input = open("test4.txt", "r").read()
+input = open("test5.txt", "r").read()
+
+
+def viz(pipes, erase=False):
+    if erase:
+        clear()
+
+    for line in pipes:
+        print(" ".join(line))
+
+    time.sleep(0.1)
 
 
 def clear():
@@ -56,9 +68,12 @@ def available_moves(input, pos):
     # Filter
     moves = filter_out_of_bounds(input, moves)
 
+    if len(moves) == 2:
+        return moves
+
     # pprint(f"Available moves from {pos} ({input[row][col]}): {moves}")
 
-    return moves
+    return []
 
 
 def connected_pipes(input, pos):
@@ -83,6 +98,61 @@ def connected_pipes(input, pos):
     return connected
 
 
+def extract_half_moves(current_pos, next_moves):
+    half_moves = map(
+        lambda x: (x[0] - current_pos[0], x[1] - current_pos[1]), next_moves
+    )
+    half_moves = list(map(lambda x: (x[0] // 2, x[1] // 2), half_moves))
+
+    return half_moves
+
+
+def magic_wand(input, current_pos, marker="~"):
+    next_moves = [current_pos]
+    seen = {}
+    count = 0
+
+    while len(next_moves) > 0:
+        current_pos, next_moves = next_moves[0], next_moves[1:]
+        moves = [
+            # Top
+            (current_pos[0] - 1, current_pos[1]),
+            # Bottom
+            (current_pos[0] + 1, current_pos[1]),
+            # Left
+            (current_pos[0], current_pos[1] - 1),
+            # Right
+            (current_pos[0], current_pos[1] + 1),
+        ]
+
+        # Filter out invalid moves
+        moves = filter(lambda x: x[0] >= 0 and x[1] >= 0, moves)
+        moves = filter(lambda x: x[0] < len(input) and x[1] < len(input[0]), moves)
+        moves = list(
+            filter(
+                lambda x: input[x[0]][x[1]] != "+" and input[x[0]][x[1]] not in markers,
+                moves,
+            )
+        )
+        moves = list(filter(lambda x: x not in seen, moves))
+
+        # Taint current pos
+        if input[current_pos[0]][current_pos[1]] == ".":
+            count += 1
+        input[current_pos[0]][current_pos[1]] = marker
+
+        # Append next moves
+        next_moves.extend(moves)
+
+        # Update seen
+        for move in moves:
+            seen[move] = True
+
+        # viz(input)
+
+    return count
+
+
 lines = input.splitlines()
 pipes = [list(line) for line in lines]
 new_pipes = []
@@ -92,9 +162,9 @@ for row in pipes:
     new_row = []
     for col in row:
         new_row.append(col)
-        new_row.append("-")
+        new_row.append(" ")
     new_pipes.append(new_row)
-    new_pipes.append(["-"] * len(new_row))
+    new_pipes.append([" "] * len(new_row))
 
 
 pipes = new_pipes
@@ -106,26 +176,73 @@ print("---------------")
 
 starting_pos = find_start(pipes)
 moves = connected_pipes(pipes, starting_pos)
+half_moves = extract_half_moves(starting_pos, moves)
+
+# Taint
+pipes[starting_pos[0]][starting_pos[1]] = str("+")
+for move in half_moves:
+    pipes[starting_pos[0] + move[0]][starting_pos[1] + move[1]] = str("+")
+
+
 moves = list(map(lambda x: (x, 1), moves))
 highest_idx = 0
 
-while len(moves) > 0:
+while True:
     (current_pos, idx), moves = moves[-1], moves[:-1]
+    if current_pos == starting_pos:
+        break
     # print("Current pos:", current_pos, pipes[current_pos[0]][current_pos[1]])
     next_moves = connected_pipes(pipes, current_pos)
+    half_moves = extract_half_moves(current_pos, next_moves)
     next_moves = list(map(lambda x: (x, idx + 1), next_moves))
-    moves.extend(next_moves)
 
-    # Taint current pos
-    pipes[current_pos[0]][current_pos[1]] = str("+")
+    if len(next_moves) > 0:
+        moves.extend(next_moves)
+
+        print("Moves", len(moves))
+
+        # Taint current pos
+        pipes[current_pos[0]][current_pos[1]] = str("+")
+        for move in half_moves:
+            pipes[current_pos[0] + move[0]][current_pos[1] + move[1]] = str("+")
 
     if idx > highest_idx:
         highest_idx = idx
 
-    clear()
-    print("---------------")
-    for line in pipes:
-        print(" ".join(line))
-    time.sleep(0.2)
+    viz(pipes, erase=True)
 
-print("Highest idx:", highest_idx)
+
+# Apply magick wand around the starting pos
+moves = [
+    # Top left
+    (starting_pos[0] - 1, starting_pos[1] - 1),
+    # Top
+    (starting_pos[0] - 1, starting_pos[1]),
+    # Top right
+    (starting_pos[0] - 1, starting_pos[1] + 1),
+    # Left
+    (starting_pos[0], starting_pos[1] - 1),
+    # Right
+    (starting_pos[0], starting_pos[1] + 1),
+    # Bottom left
+    (starting_pos[0] + 1, starting_pos[1] - 1),
+    # Bottom
+    (starting_pos[0] + 1, starting_pos[1]),
+    # Bottom right
+    (starting_pos[0] + 1, starting_pos[1] + 1),
+]
+
+# Filter out invalid moves
+moves = filter_out_of_bounds(pipes, moves)
+moves = list(filter(lambda x: pipes[x[0]][x[1]] != "+", moves))
+
+for i, move in enumerate(moves):
+    if pipes[move[0]][move[1]] in markers:
+        continue
+
+    print("---------------")
+    print("Magic wand at:", move)
+    count = magic_wand(
+        pipes, move, marker=markers[i % len(markers)]
+    )  # Divide by 2 because we doubled the space
+    print("Total count:", count)
